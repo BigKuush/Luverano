271d846a548c132efc10fc2f8fa8c8a0e53d7a4c
d-kushnarev <dm.kushnarev@yandex.ru>
2025-09-18 21:29:04 -0300

    fix: ensure consistent hero image height by standardizing background properties

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -32,7 +32,7 @@ const Hero = ({ data }: { data: HeroDataType[] }) => {
                 {
                     data.map(({ description, id, thumbnail, title }) => {
                         return (
-                            <SwiperSlide key={id} style={{ backgroundImage: `url(${thumbnail})` }} className={`hero-slide bg-no-repeat bg-center bg-cover bg-gradient-to-l from-[rgba(0,0,0,0.3)_0%] via-[rgba(0,0,0,0.1)_50%] to-[rgba(0,0,0,0.4)_100%]`}>
+                            <SwiperSlide key={id} style={{ backgroundImage: `url(${thumbnail})` }} className={`hero-slide bg-gradient-to-l from-[rgba(0,0,0,0.3)_0%] via-[rgba(0,0,0,0.1)_50%] to-[rgba(0,0,0,0.4)_100%]`}>
                                 {({ isActive }) => (
                                     <div className='container lg:pt-[190px] sm:pt-[140px] pt-30 xl:pb-[193px] lg:pb-[170px] pb-[150px]'>
                                         <motion.h1
